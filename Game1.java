dda23599cd7137bba31fc3e7b436b1ec37a21622
jchoi215 <jchoi215@uic.edu>
2017-02-26 02:15:58 +0000

    changed filename because of the interface -J.C

---
+++ Game1.java
@@ -0,0 +1,9 @@
+public class Game{
+	public static void main(String [] args){
+		Player p1 = new Player();
+		p1.playerInfo();
+		Player p2 = new Player("Tim", 15, 120, 5.0);
+		p2.playerInfo();
+
+	}
+}
