06de105e194695a593b013eb8559338d3ce6e68e
Jun Lee <jlee757@uic.edu>
2017-03-22 23:24:22 -0500

    arrow bounds

--- Arrow.java
+++ Arrow.java
@@ -62,6 +62,6 @@ public class Arrow extends GameObject{
 	}
 
 	public Rectangle getBounds() {
-		return new Rectangle ((int)x, (int)y, 16, 16);
+		return new Rectangle ((int)x, (int)y, 8, 8);
 	}
 }
